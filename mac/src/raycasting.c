933bedd6962c03b82463d5a99f6dd4ebb649c3f9
ismael hibti <gismael786@gmail.com>
2024-09-20 22:20:10 +0200

    mouais naz

--- mac/src/raycasting.c
+++ mac/src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: ihibti <ihibti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 15:22:17 by ihibti            #+#    #+#             */
-/*   Updated: 2024/09/20 22:00:42 by ihibti           ###   ########.fr       */
+/*   Updated: 2024/09/20 22:19:55 by ihibti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,6 @@ void	init_ray(t_player *player, int x)
 		ray->delta_y = fabs(1 / ray->dir_ray_y);
 }
 
-
 void	draw_line(t_ray *ray, int x, t_ori *ori)
 {
 	int		line_h;
@@ -85,26 +84,16 @@ void	draw_line(t_ray *ray, int x, t_ori *ori)
 	drawstart = (-line_h / 2) + (SCREEN_H / 2);
 	draw_end = (line_h / 2) + (SCREEN_H / 2);
 	y = 0;
-    if (ray->coord_stripe > 63)
-        ray->coord_stripe = 63.0;
+	if (ray->coord_stripe > 63)
+		ray->coord_stripe = 63.0;
 	if (drawstart < 0)
 		drawstart = 0;
 	if (draw_end >= SCREEN_H)
 		draw_end = SCREEN_H - 1;
 	if (ray->last_hit == 0)
-	{
-		if (ray->dir_ray_x > 0)
-			wall = 0;
-		else
-			wall = 3;
-	}
+		wall = 0;
 	else
-	{
-		if (ray->dir_ray_y > 0)
-			wall = 1;
-		else
-			wall = 2;
-	}
+		wall = 1;
 	while (y < drawstart && y < SCREEN_H)
 		*((int *)ori->display.data + y++ * SCREEN_W + x) = BLUE;
 	while (y <= draw_end && y < SCREEN_H)
@@ -195,37 +184,44 @@ uint32_t	get_color_mini(int x, int y, char **map, t_ori *ori)
 	return (GRAY);
 }
 
+void	drawEnemy(t_ori *ori, t_player *player, t_display *screen)
+{
+	float	dx;
+	float	dy;
+	float	distance;
+	float	angle;
+	int		screenX;
+	int		screenY;
+	int		spriteHeight;
 
-
-void drawEnemy(t_ori *ori, t_player *player, t_display *screen) {
-    // Calculate angle and distance to enemy
-    float dx = 10 - player->pos_x;
-    float dy = 5 - player->pos_y;
-    float distance = sqrt(dx*dx + dy*dy);
-    float angle = atan2(dy, dx) - player->dir_angle;
-    
-    // Adjust angle to be within FOV
-    if (angle < -M_PI) angle += 2*M_PI;
-    if (angle > M_PI) angle -= 2*M_PI;
-    
-    // Check if enemy is in view
-    if (fabs(angle) > FOV / 2) return;
-    
-    // Calculate screen position
-    int screenX = (angle + FOV/2) / FOV * screen->width;
-    int screenY = screen->height / 2;
-    
-    // Calculate enemy height on screen
-    int spriteHeight = (int)(screen->height / distance);
-    
-    // Draw sprite
-    for (int i = screenX;i < spriteHeight && i<SCREEN_W;i++)
-    {
-        for(int j = screenY;j<spriteHeight && j <SCREEN_H;j++)
-        {
-            *((int *)(screen->data + i + j*SCREEN_W)) = YELLOW;
-        }
-    } 
+	// Calculate angle and distance to enemy
+	dx = 10 - player->pos_x;
+	dy = 5 + player->pos_y;
+	distance = sqrt(dx * dx + dy * dy);
+	angle = atan2(dy, dx) - player->dir_angle;
+	// Adjust angle to be within FOV
+	if (angle < -M_PI)
+		angle += 2 * M_PI;
+	if (angle > M_PI)
+		angle -= 2 * M_PI;
+    printf("verdict >%f\n",angle);
+	// Check if enemy is in view
+	if (fabs(angle) > FOV / 2)
+		return ;
+	// Calculate screen position
+	screenX = (angle + FOV / 2) / FOV * SCREEN_W;
+	screenY = SCREEN_H / 2;
+	// Calculate enemy height on screen
+	spriteHeight = (int)(SCREEN_H / distance);
+    printf("screenX:%d,screeny:%d,spriteheight:%d\n",screenX,screenY,spriteHeight);
+	for (int i = screenX; i < spriteHeight+screenX && i < SCREEN_W; i++)
+	{
+		for (int j = screenY; j < spriteHeight+screenY && j < SCREEN_H; j++)
+		{
+			*((int *)screen->data + i + j * SCREEN_W) = YELLOW;
+		}
+	}
+    printf("success\n");
 }
 
 void	draw_minimap(t_ori *ori)
@@ -270,10 +266,8 @@ int	raycasting(t_ori *ori)
 		x++;
 	}
 	draw_minimap(ori);
-    drawEnemy(ori,ori->player,&ori->display);
+	drawEnemy(ori, ori->player, &ori->display);
 	mlx_put_image_to_window(ori->mlxptr, ori->mlxwin, ori->display.img, 0, 0);
 	ori->recast = 0;
 	return (0);
 }
-
-
